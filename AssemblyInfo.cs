c450323df8682f146d7bd2f40342f49858dc379a
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-11-22 12:48:29 -0800

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphLibrary")]
-[assembly: AssemblyDescription("Library for graph drawing")]
+[assembly: AssemblyTitle("PowerPointChartLib")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphLibrary")]
-[assembly: AssemblyCopyright("Copyright © NASA JSC/KBRwyle")]
+[assembly: AssemblyCompany("NASA ACES")]
+[assembly: AssemblyProduct("PowerPointChartLib")]
+[assembly: AssemblyCopyright("Copyright © NASA ACES 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b23e30ac-c4b8-4863-90d0-cf0ad837c050")]
+[assembly: Guid("d822054f-9e5f-4aa3-b11a-926e8ec67633")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
